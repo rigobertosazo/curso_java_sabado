3a73dbb8a41fc278fcc5ffbe9680e9c285128a8d
rigobertosazo <rigobertosazo@gmail.com>
2022-08-20 16:53:01 -0600

    Segundo commit

--- src/main/java/com/mycompany/curso_java_sabado/Curso_Java_Sabado.java
+++ src/main/java/com/mycompany/curso_java_sabado/Curso_Java_Sabado.java
@@ -8,6 +8,7 @@ public class Curso_Java_Sabado {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.print("Segundo Commit...");
        
     }
 }
