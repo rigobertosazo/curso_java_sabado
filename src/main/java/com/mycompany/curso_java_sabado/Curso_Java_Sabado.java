b91a49e46521be4e8c5bed3520492f4ffbb1b66b
rigobertosazo <rigobertosazo@gmail.com>
2022-08-20 16:26:17 -0600

    first commit

---
+++ src/main/java/com/mycompany/curso_java_sabado/Curso_Java_Sabado.java
@@ -0,0 +1,13 @@
+package com.mycompany.curso_java_sabado;
+
+/**
+ *
+ * @author Rib
+ */
+public class Curso_Java_Sabado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       
+    }
+}
